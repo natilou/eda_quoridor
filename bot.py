395e48b25920ea2fadbcd04386ec42f9ca519896
natilou <30585029+natilou@users.noreply.github.com>
2022-05-13 22:15:55 -0300

    Fixing PawnBoard and MovePawn logic

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
 from abc import ABC, abstractmethod
 import asyncio
+from urllib import request
 import websockets 
 import os
 import random
@@ -8,8 +9,8 @@ import json
 # Constants
 URL = f"wss://4yyity02md.execute-api.us-east-1.amazonaws.com/ws?token={os.getenv('auth_token')}"
 VISUAL_BOARD_WIDTH_AND_HEIGHT = 17
-PAWN_BOARD_WIDTH_AND_HEIGHT = 9
-
+SQUARE_DIMENSION = 9
+SLOT_DIMENSION = 8
 
 # Conect to websocket
 async def connect():
@@ -63,27 +64,31 @@ class Strategy(ABC):
 class MovePawn(Strategy):
     @staticmethod
     async def perform_an_action(websocket, request_data):
-        side_to_move = request_data['data']['side']
+        side = request_data['data']['side']
 
         # normalizar tablero
-        board = Board.format_board(request_data)
+        board = PawnBoard(request_data)
 
-        # falta agregar lógica para ver cuál de los 3 peones mover
-
-        # ver dónde estoy
-        for row in range(PAWN_BOARD_WIDTH_AND_HEIGHT):
-            for col in range(PAWN_BOARD_WIDTH_AND_HEIGHT):
-                if board[row][col] == side_to_move:
-                    from_row = row
-                    from_col = col
-                    to_col = col
+        # elegir uno de los tres peones:
+        selected_pawn = random.choice(board.get_my_pawns(request_data))
         
-        # decidir row a moverse
-        to_row = from_row + (1 if side_to_move == "N" else -1)
+        # ver dónde estoy
+        pawn_actual_row = selected_pawn[0] 
+        pawn_actual_col = selected_pawn[1]
 
-        # verificar que row a moverse esté vacia
-        if board[to_row][from_col] != ' ':
-            to_row = to_row + (1 if side_to_move == 'N' else -1)
+        # ver dónde moverse
+        move_to_col = ""
+        move_to_row = ""
+        direction_to_move = 1 if side == "N" else -1
+        
+        if board.get_cell(pawn_actual_row + direction_to_move, pawn_actual_col) == " ":
+            move_to_row = pawn_actual_row + direction_to_move
+            move_to_col = pawn_actual_col 
+        
+        elif board.get_cell(pawn_actual_row + direction_to_move, pawn_actual_col) != " ":
+            move_to_row = pawn_actual_row + direction_to_move * 2
+            move_to_col = pawn_actual_col         
+
 
         await Messenger.send(
             websocket, 
@@ -91,10 +96,10 @@ class MovePawn(Strategy):
             {
                 'game_id': request_data['data']['game_id'], 
                 'turn_token': request_data['data']['turn_token'], 
-                'from_row': from_row,
-                'from_col': from_col,
-                'to_row': to_row,
-                'to_col': to_col,
+                'from_row': pawn_actual_row,
+                'from_col': pawn_actual_col,
+                'to_row': move_to_row,
+                'to_col': move_to_col,
             }
         )
                 
@@ -146,21 +151,90 @@ class Game:
             raise Exception("No allowed strategies found")
        
         selected_strategy = random.choice(allowed_strategies)
+        
         await selected_strategy.perform_an_action(websocket, request_data)
 
-class Board:
-    def format_board(request_data):
+
+class PawnBoard:
+    def __init__(self, request_data):
+        self.pawn_board = self.format_pawn_board(request_data)
+
+    def format_pawn_board(self, request_data):
         board_data = request_data['data']['board']
         pawn_board = []
         length_row = VISUAL_BOARD_WIDTH_AND_HEIGHT
         num_row = 0
-        for row in range(9):
-            board_data_row = [board_data[num_row : length_row]]
-            pawn_board.append(board_data_row)
-            num_row = length_row
-            length_row += VISUAL_BOARD_WIDTH_AND_HEIGHT
+
+        for row in range(SQUARE_DIMENSION):
+            board_data_row = board_data[num_row : length_row : 2]
+            pawn_board.append(list(board_data_row))
+            if row != SQUARE_DIMENSION - 1:
+                num_row = length_row + VISUAL_BOARD_WIDTH_AND_HEIGHT
+                length_row += VISUAL_BOARD_WIDTH_AND_HEIGHT + VISUAL_BOARD_WIDTH_AND_HEIGHT
+            else:
+                num_row = len(board_data) - VISUAL_BOARD_WIDTH_AND_HEIGHT
+                length_row = len(board_data)
+
         return pawn_board
 
+    def get_pawns(self, side):
+        pawns = []
+        for row in range(SQUARE_DIMENSION):
+            for col in range(SQUARE_DIMENSION):
+                if self.pawn_board[row][col] == side:
+                    pawns.append((row, col)) 
+        
+        print(f"pawns {pawns}")       
+        return pawns
+    
+    def get_my_pawns(self, request_data):
+        my_side = request_data['data']['side']
+        my_pawns = self.get_pawns(my_side)
+
+        return my_pawns
+    
+    def get_opponent_paws(self, request_data):
+        opponent_side = "S" if request_data['data']['side'] == "N" else "N"
+        opponent_pawns = self.get_pawns(opponent_side)
+
+        return opponent_pawns
+
+    def get_cell(self, row, col):
+        return self.pawn_board[row][col]
+       
+
+class WallBoard:
+    def __init__(self, request_data):
+        self.wall_board = self.format_wall_board(request_data) 
+
+    def format_wall_board(self, request_data):
+        board_data = request['data']['board']
+        wall_board = []
+        length_row = VISUAL_BOARD_WIDTH_AND_HEIGHT * 2
+        num_row = VISUAL_BOARD_WIDTH_AND_HEIGHT
+
+        for row in range(SLOT_DIMENSION):
+            board_data_row = board_data[num_row : length_row : 2]
+            wall_board.append(list(board_data_row))
+            if row != SLOT_DIMENSION - 1:
+                num_row = length_row + VISUAL_BOARD_WIDTH_AND_HEIGHT
+                length_row += VISUAL_BOARD_WIDTH_AND_HEIGHT + VISUAL_BOARD_WIDTH_AND_HEIGHT
+            else:
+                num_row = len(board_data) - VISUAL_BOARD_WIDTH_AND_HEIGHT
+                length_row = len(board_data)
+
+        return wall_board
+
+    # def get_walls(self, side):
+    #     walls = []
+    #     for row in range(9):
+    #         for col in range(9):
+    #             if self.wall_board[row][col] == side:
+    #                 walls.append((row, col)) 
+        
+    #     print(f"pawns {walls}")       
+    #     return walls
+    
 
 # Send message to websocket
 class Messenger:
